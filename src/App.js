6b6741f6694abb9f12d32c9de107e42e1567bfac
Jeff Juszczak <jeff@spectrio.com>
2018-12-29 10:34:15 -0500

    Change welcome message

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            I was changed again!!!
+            Try a change and redeploy.
           </p>
           <a
             className="App-link"
